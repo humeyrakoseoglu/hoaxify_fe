b3064bfa551b7b092bd23b31ce09516a6e54eaf9
humeyrakoseoglu <20180808015@ogr.akdeniz.edu.tr>
2023-09-04 00:50:27 +0300

    created api folder

--- src/pages/signUp/index.js
+++ src/pages/signUp/index.js
@@ -1,53 +1,88 @@
 import React, { Component } from 'react';
 import './index.css';
-import axios from 'axios';
-class UserSignUpPage extends Component {
+import { signUp } from '../../api/apiCalls';
 
-  // Bileşen içinde kullanacağımız durumu tanımlıyoruz
+class UserSignUpPage extends Component {
   state = {
-    username: null,
+    userName: null,
     fullName: null,
     password: null,
     passwordRepeat: null,
+    pendingApiCall: false,
   };
 
   handleInputChange = (e) => {
     const { name, value } = e.target;
-    // [name] ifadesi, hangi input alanının güncellendiğini belirler (örneğin, "username")
-    // value ise input alanına girilen yeni değeri temsil eder
     this.setState({ [name]: value });
   };
 
-  signUp = (e) => {
+  signUp = async (e)  =>  {
     e.preventDefault();
 
-    const {username, fullName, password} = this.state;
+    const { userName, fullName, password } = this.state;
 
-    const body={
-      username,
+    const body = {
+      userName,
       fullName,
       password,
-    }
+    };
 
-    axios.post('/api/1.0/users/createUser', body);
-  }
+    this.setState({ pendingApiCall: true });
+
+    try {
+      const response = await signUp(body);
+    } catch (error) {}
+    this.setState({ pendingApiCall: false });
+  };
 
   render() {
+    const { pendingApiCall } = this.state;
 
     return (
       <div className="signUp-container">
         <h1>Sign Up</h1>
         <form className="signUp-form">
-          <input type="text" onChange={this.handleInputChange} placeholder="Username" name="username" className="input-field" />
-          <input type="text"  onChange={this.handleInputChange} placeholder="Full Name" name="fullName" className="input-field" />
-          <input type="password"  onChange={this.handleInputChange} placeholder="Password" name="password" className="input-field" />
-          <input type="password"  onChange={this.handleInputChange} placeholder="Password Repeat" name="passwordRepeat" className="input-field" />
-          <button type="submit" className="submit-button" onClick={this.signUp}>Sign Up</button>
+          <input
+            type="text"
+            onChange={this.handleInputChange}
+            placeholder="Username"
+            name="userName"
+            className="input-field"
+          />
+          <input
+            type="text"
+            onChange={this.handleInputChange}
+            placeholder="Full Name"
+            name="fullName"
+            className="input-field"
+          />
+          <input
+            type="password"
+            onChange={this.handleInputChange}
+            placeholder="Password"
+            name="password"
+            className="input-field"
+          />
+          <input
+            type="password"
+            onChange={this.handleInputChange}
+            placeholder="Password Repeat"
+            name="passwordRepeat"
+            className="input-field"
+          />
+          <button
+            type="button"
+            className="btn submit-button"
+            onClick={this.signUp}
+            disabled={pendingApiCall}
+          >
+            {pendingApiCall ? <span className="spinner-border spinner-border-sm"></span>: ''}
+            Sign Up
+          </button>
         </form>
       </div>
     );
   }
-
 }
 
 export default UserSignUpPage;
